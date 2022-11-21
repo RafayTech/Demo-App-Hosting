6b641d8bcbf0b4bde55969e27d932e35c681e13c
Abdul Rafay Ahmad <abdulrafay1172@gmail.com>
2022-11-21 19:09:41 +0500

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FacebookService, InitParams } from 'ngx-facebook';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'demo-app';
+export class AppComponent implements OnInit {
+  constructor(private facebookService: FacebookService) { }
+    ngOnInit(): void {
+      this.initFacebookService();
+    }
+    private initFacebookService(): void {
+      const initParams: InitParams = { xfbml:true, version:'v15.0'};
+      this.facebookService.init(initParams);
+    }
 }
